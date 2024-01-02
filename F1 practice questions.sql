8c36fedae0730f185815e6054455f2dcb6cbaeb0
Sakif Chowdhury <35176851+sakifchowdhury1@users.noreply.github.com>
2024-01-02 12:23:27 -0500

    Update F1 practice questions.sql

--- F1 practice questions.sql
+++ F1 practice questions.sql
@@ -23,7 +23,7 @@ WHERE r.fastestlapspeed IS NOT NULL;
 SELECT d.driverid, d.forename, d.surname, max(r.fastestlapspeed) as "Fastest Speed"
 FROM drivers AS d
 JOIN results AS r ON d.driverid = r.driverid
-WHERE r.fastestlapspeed is not null
+WHERE r.fastestlapspeed IS NOT null
 GROUP BY d.driverid, d.forename, d.surname;
 
 
@@ -53,40 +53,40 @@ FROM drivers;
 
 
 --drivers and fastest lap speed in Monaco 2021
-SELECT d.driverid, d.forename, d.surname, max(r.fastestlapspeed) as "Fastest Speed",
+SELECT d.driverid, d.forename, d.surname, max(r.fastestlapspeed) AS "Fastest Speed",
 (SELECT max(fastestlapspeed) FROM results)
-FROM drivers as d
-JOIN results as r on d.driverid = r.driverid
-JOIN races on r.raceid = races.raceid
+FROM drivers AS d
+JOIN results AS r ON d.driverid = r.driverid
+JOIN races ON r.raceid = races.raceid
 WHERE r.fastestlapspeed is not null 
-    and races.name ilike '%Monaco%'
-    and races.year = 2021
+    AND races.name ilike '%Monaco%'
+    AND races.year = 2021
 GROUP BY d.driverid, d.forename, d.surname
-ORDER BY max(r.fastestlapspeed) desc;
+ORDER BY max(r.fastestlapspeed) DESC;
 
 
 
 
 -- per driver, fastest lap speed monaco 2021 and how many times they won in monaco before
 SELECT d.driverid, d.forename, d.surname, max(r.fastestlapspeed) as "Fastest Speed", total_wins
-FROM drivers as d
-JOIN results as r on d.driverid = r.driverid
-JOIN races on r.raceid = races.raceid
+FROM drivers AS d
+JOIN results AS r ON d.driverid = r.driverid
+JOIN races ON r.raceid = races.raceid
 JOIN (
-    SELECT standings.driverid, sum(standings.wins) as total_wins
-    FROM driver_standings as standings 
-    JOIN races on races.raceid = standings.raceid
+    SELECT standings.driverid, sum(standings.wins) AS total_wins
+    FROM driver_standings AS standings 
+    JOIN races ON races.raceid = standings.raceid
     WHERE races.name ilike '%Monaco%'
-    GROUP BY standings.driverid) as Wins on d.driverid = Wins.driverid
+    GROUP BY standings.driverid) AS Wins ON d.driverid = Wins.driverid
 WHERE r.fastestlapspeed is not null 
-    and races.name ilike '%Monaco%'
-    and races.year = 2021
+    AND races.name ilike '%Monaco%'
+    AND races.year = 2021
 GROUP BY d.driverid, d.forename, d.surname, total_wins;
 
 
 SELECT standings.driverid, sum(standings.wins) 
-FROM driver_standings as standings 
-JOIN races on races.raceid = standings.raceid
+FROM driver_standings AS standings 
+JOIN races ON races.raceid = standings.raceid
 WHERE races.name ilike '%Monaco%'
 GROUP BY standings.driverid;
 
@@ -101,7 +101,7 @@ WITH pts
     
 SELECT drivers.driverid, pts.*
 FROM drivers
-JOIN pts on pts.driverid = drivers.driverid;
+JOIN pts ON pts.driverid = drivers.driverid;
 
 SELECT driverid, sum(points)
 FROM results 
@@ -112,20 +112,20 @@ GROUP BY driverid;
 
 -- list of drivers with fastest lap speeds in monaco in2021 and how many times they won  (with CTE)
 WITH wins AS
-    (SELECT driverid, sum(wins) as numbers
-    FROM driver_standings as standings
-    JOIN races on standings.raceid = races.raceid
+    (SELECT driverid, sum(wins) AS numbers
+    FROM driver_standings AS standings
+    JOIN races ON standings.raceid = races.raceid
     WHERE races.name ilike '%Monaco%' 
     GROUP BY driverid)
 
-SELECT d.driverid, d.forename, d.surname, max(r.fastestlapspeed) as "Fastest Speed", max(numbers)
-FROM drivers as d
-JOIN results as r on d.driverid = r.driverid
-JOIN races on r.raceid = races.raceid
-JOIN wins on wins.driverid = d.driverid
-WHERE r.fastestlapspeed is not null 
-    and races.name ilike '%Monaco%'
-    and races.year = 2021
+SELECT d.driverid, d.forename, d.surname, max(r.fastestlapspeed) AS "Fastest Speed", max(numbers)
+FROM drivers AS d
+JOIN results AS r ON d.driverid = r.driverid
+JOIN races ON r.raceid = races.raceid
+JOIN wins ON wins.driverid = d.driverid
+WHERE r.fastestlapspeed IS NOT null 
+    AND races.name ilike '%Monaco%'
+    AND races.year = 2021
 GROUP BY d.driverid, d.forename, d.surname;
 
 
@@ -143,29 +143,29 @@ SELECT n FROM numbers;
 
 
 -- list of drivers fastest lap speed in monaco in 2021 and 2019
-WITH results_2021 as 
+WITH results_2021 AS 
        (SELECT d.driverid, d.forename, d.surname, max(r.fastestlapspeed) as fastest_2021
-        FROM drivers as d
-        JOIN results as r on d.driverid = r.driverid
-        JOIN races on r.raceid = races.raceid
+        FROM drivers AS d
+        JOIN results AS r ON d.driverid = r.driverid
+        JOIN races ON r.raceid = races.raceid
         WHERE r.fastestlapspeed is not null 
-            and races.name ilike '%Monaco%'
-            and races.year = 2021
+            AND races.name ilike '%Monaco%'
+            AND races.year = 2021
         GROUP BY d.driverid, d.forename, d.surname
-        ORDER BY max(r.fastestlapspeed) desc
+        ORDER BY max(r.fastestlapspeed) DESC
         ),
-    results_2019 as
-       (SELECT d.driverid, d.forename, d.surname, max(r.fastestlapspeed) as fastest_2019
-        FROM drivers as d
-        JOIN results as r on d.driverid = r.driverid
-        JOIN races on r.raceid = races.raceid
+    results_2019 AS
+       (SELECT d.driverid, d.forename, d.surname, max(r.fastestlapspeed) AS fastest_2019
+        FROM drivers AS d
+        JOIN results AS r ON d.driverid = r.driverid
+        JOIN races ON r.raceid = races.raceid
         WHERE r.fastestlapspeed is not null 
-            and races.name ilike '%Monaco%'
-            and races.year = 2019
+            AND races.name ilike '%Monaco%'
+            AND races.year = 2019
         GROUP BY d.driverid, d.forename, d.surname
-        ORDER BY max(r.fastestlapspeed) desc
+        ORDER BY max(r.fastestlapspeed) DESC
         )
 SELECT drivers.driverid, drivers.forename, drivers.surname, fastest_2021, fastest_2019
 FROM drivers
-JOIN results_2021 on results_2021.driverid = drivers.driverid
-JOIN results_2019 on results_2019.driverid = drivers.driverid ;
+JOIN results_2021 ON results_2021.driverid = drivers.driverid
+JOIN results_2019 ON results_2019.driverid = drivers.driverid ;
